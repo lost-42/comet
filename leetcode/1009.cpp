1291f2fa978844568f3a7e21426a337651d93a28
Ollie <rui96uk@gmail.com>
2026-03-11 17:18:31 +0800

    lc 1009

---
+++ leetcode/1009.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if (n == 0)
+            return 1;
+
+        int ans = 0;
+        int p = 0;
+        while (n > 0) {
+            int t = !(n & 1);
+            ans += t << p;
+            n = n >> 1;
+            ++p;
+        }
+
+        return ans;
+    }
+};
+
+
+int main() {
+    Solution s{};
+
+    std::cout << s.bitwiseComplement(5) << std::endl;
+    std::cout << s.bitwiseComplement(7) << std::endl;
+    std::cout << s.bitwiseComplement(10) << std::endl;
+}
--- leetcode/1784.cpp
+++ leetcode/1784.cpp
@@ -1,32 +1,32 @@
-import <string>;
-import <iostream>;
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 class Solution {
 public:
-  bool checkOnesSegment(const string &s) {
-    if (s.empty())
-      return true;
+    bool checkOnesSegment(const string& s) {
+        if (s.empty())
+            return true;
 
-    int oneCnt = (s[0] == '1') ? 1 : 0;
-    for (int i = 1; i < s.size(); ++i) {
-      if (s[i] == '1' && s[i - 1] == '0') {
-        ++oneCnt;
-      }
-      if (oneCnt > 1)
-        return false;
+        int oneCnt = (s[0] == '1') ? 1 : 0;
+        for (size_t i = 1; i < s.size(); ++i) {
+            if (s[i] == '1' && s[i - 1] == '0') {
+                ++oneCnt;
+            }
+            if (oneCnt > 1)
+                return false;
+        }
+
+        return oneCnt <= 1;
     }
-
-    return oneCnt <= 1;
-  }
 };
 
 int main() {
-  Solution s{};
-  std::cout << s.checkOnesSegment("1001") << std::endl; // 应该输出0 (false)
-  std::cout << s.checkOnesSegment("110") << std::endl;  // 应该输出1 (true)
-  std::cout << s.checkOnesSegment("1") << std::endl;    // 应该输出1 (true)
-  std::cout << s.checkOnesSegment("10") << std::endl;   // 应该输出1 (true)
-  std::cout << s.checkOnesSegment("101") << std::endl;  // 应该输出0 (false)
+    Solution s{};
+    std::cout << s.checkOnesSegment("1001") << std::endl;  // 应该输出0 (false)
+    std::cout << s.checkOnesSegment("110") << std::endl;   // 应该输出1 (true)
+    std::cout << s.checkOnesSegment("1") << std::endl;     // 应该输出1 (true)
+    std::cout << s.checkOnesSegment("10") << std::endl;    // 应该输出1 (true)
+    std::cout << s.checkOnesSegment("101") << std::endl;   // 应该输出0 (false)
 }
